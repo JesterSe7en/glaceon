5ff99008f33aabe75a5a2b8113c9215b1463e7fd
Alex Chan <alyxchan87@gmail.com>
2024-02-28 14:49:09 -0800

    Add check for vulkan

--- Glaceon/Glaceon.cpp
+++ Glaceon/Glaceon.cpp
@@ -1,26 +1,51 @@
-#include "pch.h"
 #include "Glaceon.h"
-#include <iostream>
+#include "pch.h"
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <cstdio>
 
 namespace Glaceon {
 
+void error_callback(int error, const char *description) {
+  fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
 void GLACEON_API runGame(Application *app) {
   if (!app) {
-    std::cout << "Application is null" << std::endl;
+    fprintf(stdout, "Application is null\n");
     return;
+  }
+
+  fprintf(stdout, "Checking for vulkan support...\n");
+
+  bool vulkanSupported = glfwVulkanSupported();
+  if (vulkanSupported) {
+    fprintf(stdout, "Vulkan supported\n");
+  } else {
+    fprintf(stdout, "Vulkan not supported\n");
   }
 
   if (!glfwInit()) {
-    std::cout << "GLFW initialization failed" << std::endl;
+    fprintf(stdout, "GLFW initialization failed\n");
     return;
   }
 
+  glfwSetErrorCallback(error_callback);
 
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "GLFW Test Window", nullptr, nullptr);
+
+  if (!window) {
+    fprintf(stdout, "GLFW window creation failed\n");
+    return;
+  } else {
+    fprintf(stdout, "GLFW window created\n");
+  }
 
   app->onStart();
   app->onUpdate();
 
+  glfwDestroyWindow(window);
   glfwTerminate();
   app->onShutdown();
 }
