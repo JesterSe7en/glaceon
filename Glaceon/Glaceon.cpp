f10495dc54f9e95de41a4ba15dec6c6ea82a8f84
Alex Chan <alyxchan87@gmail.com>
2024-02-29 00:04:07 -0800

    Working spdlog - need to change spdlog to build shared libs

--- Glaceon/Glaceon.cpp
+++ Glaceon/Glaceon.cpp
@@ -4,6 +4,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <cstdio>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace Glaceon {
 
@@ -12,6 +14,8 @@ void error_callback(int error, const char *description) {
 }
 
 void GLACEON_API runGame(Application *app) {
+  auto console = spdlog::stdout_color_mt("console");
+  spdlog::get("console")->info("SPDLOG: running game...");
   if (!app) {
     fprintf(stdout, "Application is null\n");
     return;
@@ -32,7 +36,7 @@ void GLACEON_API runGame(Application *app) {
 
   glfwSetErrorCallback(error_callback);
 
-  auto pfnCreateInstance = (PFN_vkCreateInstance)glfwGetInstanceProcAddress(
+  auto pfnCreateInstance = (PFN_vkCreateInstance) glfwGetInstanceProcAddress(
       nullptr, "vkCreateInstance");
 
   // we are using vulkan, don't load in other apis
