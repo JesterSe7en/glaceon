6dd25f826d5383f7d389a8c073198c35f41fc876
Alex Chan <alyxchan87@gmail.com>
2024-03-01 06:25:23 -0800

    WIP - adding imgui ui

--- Glaceon/Glaceon.cpp
+++ Glaceon/Glaceon.cpp
@@ -2,9 +2,14 @@
 #include "Logger.h"
 #include "pch.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+
+#define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan.h>
 
 namespace Glaceon {
 
