208dcb1fab9880272348d9aab1686ee786e2cfea
Alex Chan <alyxchan87@gmail.com>
2024-03-25 11:38:32 -0700

    More cast warnings

--- Glaceon/VulkanRenderer/VulkanCommandPool.cpp
+++ Glaceon/VulkanRenderer/VulkanCommandPool.cpp
@@ -74,7 +74,8 @@ void VulkanCommandPool::RebuildCommandBuffers() {
 
   // destroy all old command buffers
   device.freeCommandBuffers(vk_command_pool_, 1, &vk_main_command_buffer_);
-  device.freeCommandBuffers(vk_command_pool_, vk_frame_command_buffers_.size(), vk_frame_command_buffers_.data());
+  device.freeCommandBuffers(vk_command_pool_, static_cast<uint32_t>(vk_frame_command_buffers_.size()),
+                            vk_frame_command_buffers_.data());
 
   vk_frame_command_buffers_.clear();
   std::vector<SwapChainFrame> swap_chain_frames = context_.GetVulkanSwapChain().GetSwapChainFrames();
