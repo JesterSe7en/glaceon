b9dab5736e7f6bf3ff483943060b61d868ff25c5
Alex Chan <alyxchan87@gmail.com>
2024-04-04 15:30:06 -0700

    Adjust positioning of meshes

--- Glaceon/Scene.cpp
+++ Glaceon/Scene.cpp
@@ -2,13 +2,14 @@
 
 namespace glaceon {
 Scene::Scene() {
-  float x = -0.6f;
+
+  float x = -0.3f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { triangle_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 
   x = 0.0f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { square_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 
-  x = 0.6f;
+  x = 0.3f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { star_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 }
 }// namespace glaceon
