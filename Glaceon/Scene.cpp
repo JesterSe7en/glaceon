7e7545daf091ef71cad62651ff6a7e4bb037ddd1
Alex Chan <alyxchan87@gmail.com>
2024-04-08 10:00:28 -0700

    Pack them closer together

--- Glaceon/Scene.cpp
+++ Glaceon/Scene.cpp
@@ -3,13 +3,13 @@
 namespace glaceon {
 Scene::Scene() {
 
-  float x = -0.3f;
+  float x = -0.2f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { triangle_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 
   x = 0.0f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { square_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 
-  x = 0.3f;
+  x = 0.2f;
   for (float y = -1.0f; y < 1.0f; y += 0.2f) { star_positions_.push_back(glm::vec3(x, y, 0.0f)); }
 }
 }// namespace glaceon
