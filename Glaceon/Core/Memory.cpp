5bc13a6c39c270868ea3737c3e0d852565d136a6
Alex Chan <alyxchan87@gmail.com>
2024-05-13 14:36:11 -0700

    WIP - add linear allocator
    Still working in progress = stack allocator

--- Glaceon/Core/Memory.cpp
+++ Glaceon/Core/Memory.cpp
@@ -1,10 +1,9 @@
 #include "Memory.h"
-#include "Logger.h"
-#include <winbase.h>
 
-#define WIN32_LEAN_AND_MEAN
+#include <cstddef>
 #include <cstring>
-#include <windows.h>
+
+#include "Logger.h"
 
 namespace glaceon {
 
@@ -13,13 +12,7 @@ struct memory_stats {
   uint64_t tagged_allocations[MEMORY_TAG_MAX_TAGS];
 };
 
-static std::string tag_names[MEMORY_TAG_MAX_TAGS] = {
-    "Unknown",
-    "Array",
-    "DynArray",
-    "Dict",
-    "String",
-    "Texture"};
+static std::string tag_names[MEMORY_TAG_MAX_TAGS] = {"Unknown", "Array", "DynArray", "Dict", "String", "Texture"};
 
 static memory_stats stats_ = {0, {0, 0, 0, 0, 0, 0}};
 
@@ -31,9 +24,7 @@ MemorySubsystem::MemorySubsystem() {
 MemorySubsystem::~MemorySubsystem() {}
 
 void *MemorySubsystem::GAllocate(uint64_t size, MemoryTag tag) {
-  if (tag == MEMORY_TAG_UNKNOWN) {
-    GWARN("GAllocate called with MEMORY_TAG_UNKNOWN.  Specifying a tag is recommended.");
-  }
+  if (tag == MEMORY_TAG_UNKNOWN) { GWARN("GAllocate called with MEMORY_TAG_UNKNOWN.  Specifying a tag is recommended."); }
 
   // update stats
   stats_.total_allocated += size;
@@ -42,36 +33,25 @@ void *MemorySubsystem::GAllocate(uint64_t size, MemoryTag tag) {
   GTRACE("Tagged allocations: {}", tag_names[tag]);
 
   // TODO: allow for alignment bool?
-  // Following only works on Windows
-  return (void *) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);// also zeroes memory
+  return GZeroMemory(malloc(size), size);
 }
 
 void MemorySubsystem::GFree(void *mem_block, uint64_t size, MemoryTag tag) {
-  if (tag == MEMORY_TAG_UNKNOWN) {
-    GWARN("GFree called with MEMORY_TAG_UNKNOWN.  Specifying a tag is recommended.");
-  }
+  if (tag == MEMORY_TAG_UNKNOWN) { GWARN("GFree called with MEMORY_TAG_UNKNOWN.  Specifying a tag is recommended."); }
 
   // update stats
   stats_.total_allocated -= size;
   stats_.tagged_allocations[tag] -= size;
 
   // TODO: allow for alignment bool?
-  HeapFree(GetProcessHeap(), 0, mem_block);
+  free(mem_block);
 }
 
-// Following functions are just wrappers around standard C functions
+void *MemorySubsystem::GZeroMemory(void *mem_block, uint64_t size) { return memset(mem_block, 0, size); }
 
-void *MemorySubsystem::GZeroMemory(void *mem_block, uint64_t size) {
-  return memset(mem_block, 0, size);
-}
+void *MemorySubsystem::GCopyMemory(void *dest, const void *src, uint64_t size) { return memcpy(dest, src, size); }
 
-void *MemorySubsystem::GCopyMemory(void *dest, const void *src, uint64_t size) {
-  return memcpy(dest, src, size);
-}
-
-void *MemorySubsystem::GSetMemory(void *dest, int value, uint64_t size) {
-  return memset(dest, value, size);
-}
+void *MemorySubsystem::GSetMemory(void *dest, int value, uint64_t size) { return memset(dest, value, size); }
 
 // TODO: convert this to return a string
 void MemorySubsystem::PrintStats() {
@@ -93,6 +73,114 @@ void MemorySubsystem::PrintStats() {
       GTRACE("{}: {:03.2f} {}", tag_names[i], static_cast<float>(stats_.tagged_allocations[i]), "B");
     }
   }
+}
+
+// -------------------------- LINEAR ALLOCATOR --------------------------
+
+// Initializes the LinearAllocator with the given size and start address.
+// Typically, malloc is called prior to wrapping the malloc block into a LinearAllocator.
+
+// e.g.
+// 	size_t memory_size = 1024ULL * 1024 * 1024; //1GB
+//	_memory            = malloc(memory_size);
+// 	_main_allocator = new (_memory)LinearAllocator(memory_size - sizeof(FreeListAllocator),
+
+LinearAllocator::LinearAllocator(size_t size, void *start) {
+  current_pos_ = start;
+  start_ = start;
+  size_ = size;
+  used_memory_ = 0;
+  assert(size > 0);// Ensure that size is greater than 0
+}
+
+LinearAllocator::~LinearAllocator() { current_pos_ = nullptr; }
+
+/**
+ * @brief Allocates a block of memory from the LinearAllocator.
+ *
+ * @param size The size of the memory block to allocate.
+ * @param alignment The alignment of the memory block.
+ * @return void* A pointer to the allocated memory block, or nullptr if the allocation failed.
+ */
+void *LinearAllocator::Allocate(size_t size, uint8_t alignment) {
+  assert(size != 0);
+  uintptr_t align_address = AlignAddress(reinterpret_cast<uintptr_t>(current_pos_), alignment);
+  if (used_memory_ + size > size_) return nullptr;
+
+  current_pos_ = (void *) (align_address + size);
+  used_memory_ += size;
+  return (void *) align_address;
+}
+
+/**
+ * @brief Clears the LinearAllocator, resetting the current position to the start and setting the used memory to 0.
+ *
+ * This function is used to reset the LinearAllocator to its initial state. It is typically called when you want to reuse the LinearAllocator for a new allocation.
+ */
+void LinearAllocator::Clear() {
+  current_pos_ = start_;
+  used_memory_ = 0;
+}
+
+uintptr_t LinearAllocator::AlignAddress(uintptr_t address, uint32_t alignment) {
+  const size_t kM = alignment - 1;
+  return (address + kM) & ~kM;
+}
+
+// -------------------------- STACK ALLOCATOR --------------------------
+StackAllocator::StackAllocator(size_t size, void *start) {
+  assert(size > 0);// Ensure that size is greater than 0
+
+#if _DEBUG
+  prev_position_ = nullptr;
+#endif// _DEBUG
+  prev_position_ = nullptr;
+
+  current_pos_ = start;
+  start_ = start;
+  size_ = size;
+  used_memory_ = 0;
+}
+
+StackAllocator::~StackAllocator() {
+#if _DEBUG
+  prev_position_ = nullptr;
+#endif// _DEBUG
+  prev_position_ = nullptr;
+  current_pos_ = nullptr;
+}
+
+void *StackAllocator::Allocate(size_t size, uint8_t alignment) {
+  assert(size != 0);
+  uintptr_t align_address = AlignAddress(reinterpret_cast<uintptr_t>(current_pos_), alignment);
+
+  if (used_memory_ + size > size_) return nullptr;
+
+  // Prepend the AllocationHeader to the current position
+  auto *header = (AllocationHeader *) (&align_address - sizeof(AllocationHeader));
+  header->alignment = alignment;
+
+#if _DEBUG
+  header->prev_address_ = prev_position_;
+  prev_position_ = align_address;
+#endif// _DEBUG
+
+  current_pos_ = align_address;
+  used_memory_ += size;
+
+  return align_address;
+}
+
+void StackAllocator::Deallocate(void *ptr) {
+  assert(ptr == prev_position_);
+
+  auto *header = (AllocationHeader *) (&ptr - sizeof(AllocationHeader));
+  /
+}
+
+uintptr_t StackAllocator::AlignAddress(uintptr_t address, uint32_t alignment) {
+  const size_t kM = alignment - 1;
+  return (address + kM) & ~kM;
 }
 
 }// namespace glaceon
--- Glaceon/Core/Memory.h
+++ Glaceon/Core/Memory.h
@@ -26,8 +26,54 @@ class MemorySubsystem {
   static void *GZeroMemory(void *mem_block, uint64_t size);
   static void *GCopyMemory(void *dest, const void *src, uint64_t size);
   static void *GSetMemory(void *dest, int value, uint64_t size);
+  static uintptr_t AlignAddress(uintptr_t address, uint32_t alignment);
 
   static void PrintStats();
+};
+
+class LinearAllocator {
+ public:
+  LinearAllocator(size_t size, void *start);
+  ~LinearAllocator();
+
+  void *Allocate(size_t size, uint8_t alignment);
+  void Clear();
+
+ private:
+  uintptr_t AlignAddress(uintptr_t address, uint32_t alignment);
+  void *current_pos_;
+  void *start_;
+  size_t size_;
+  size_t used_memory_;
+};
+
+class StackAllocator {
+ public:
+  StackAllocator(size_t size, void *start);
+  ~StackAllocator();
+
+  void *Allocate(size_t size, uint8_t alignment);
+  void Deallocate(void *ptr);
+  void Clear();
+
+ private:
+  uintptr_t AlignAddress(uintptr_t address, uint32_t alignment);
+  void *current_pos_;
+  void *start_;
+  size_t size_;
+  size_t used_memory_;
+
+#if _DEBUG
+  void *prev_position_;
+#endif// _DEBUG
+
+  struct AllocationHeader {
+#if _DEBUG
+    void *prev_address_;
+#endif// _DEBUG
+
+    uint8_t alignment;
+  };
 };
 
 }// namespace glaceon
